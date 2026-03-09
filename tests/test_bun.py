d5d0f4cfb82b3ea06523539908af7c8efff2a85c
Kirill Martyushov <martyushov.k.i.m@gmail.com>
2026-03-10 02:39:08 +0300

    Добавление атомарности в тесты: разбил тест на 2 проверки

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,13 +1,19 @@
 import pytest
 
-
 from praktikum.bun import Bun
 from tests.data import BUN
 
+
 class TestBun:
+
     @pytest.mark.parametrize('name, price', BUN)
-    def test_bun_getters(self, name, price):
+    def test_get_name_returns_correct_name(self, name, price):
         bun = Bun(name, price)
 
         assert bun.get_name() == name
+
+    @pytest.mark.parametrize('name, price', BUN)
+    def test_get_price_returns_correct_price(self, name, price):
+        bun = Bun(name, price)
+
         assert bun.get_price() == price
